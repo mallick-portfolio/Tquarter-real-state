ebfbb8bae87126c4be691d7c86421e0a11da8db7
mallick-portfolioE <tamal1714019@gmail.com>
2022-06-30 19:09:35 +0600

    Added header

---
+++ components/shared/header.js
@@ -0,0 +1,34 @@
+import Image from "next/image.js";
+import Logo from "../../public/images/logo.png";
+const Header = () => {
+  return (
+    <div className=" py-5 bg-[#f2f6f7]">
+      <navbar className="flex justify-between items-center container mx-auto">
+        <div>
+          <Image src={Logo} alt={"Logo"} />
+        </div>
+        <nav>
+          <ul className="flex items-center text-lg">
+            <li className="px-5 py-2 text-primary font-semibold">
+              <a href="">Home</a>
+            </li>
+            <li className="px-5 py-2 text-primary font-semibold">
+              <a href="">About</a>
+            </li>
+            <li className="px-5 py-2 text-primary font-semibold">
+              <a href="">Shop</a>
+            </li>
+            <li className="px-5 py-2 text-primary font-semibold">
+              <a href="">News</a>
+            </li>
+            <li className="px-5 py-2 text-primary font-semibold">
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </navbar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#071c1f',
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "3rem",
+          xl: "4rem",
+          "2xl": "5rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
