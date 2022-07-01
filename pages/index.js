c75463fe620848011f004298fec356c36baf1cbc
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-01 17:28:19 +0600

    update ...

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 
+import Banner from "../components/banner/banner.js";
 import Layout from "../components/shared/layout.js";
 
 export default function Home() {
   return (
     <div>
       <Layout>
-        <main>Hello world</main>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo provident eaque tempora eos repudiandae. Iusto facere saepe molestias consequatur quod dolorem reprehenderit corporis libero delectus maxime! Illo ad natus ratione!</div>
+        <Banner />
       </Layout>
     </div>
   );
