44d9af3002e0995bcbdb4f59ee48cb7ac2d3a388
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-05 14:22:03 +0600

    added navigation button

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper";
 import Image from "next/image.js";
 import slide1 from "../../public/images/61.jpg";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
 export default function Banner() {
   return (
     <div className="">
-      <Swiper autoplay={5000} speed={800} loop={true} className="mySwiper">
+      <Swiper
+        autoplay={{ delay: 5000 }}
+        navigation={{
+          prevEl: ".prev",
+          nextEl: ".next",
+        }}
+        loop={true}
+        pagination={true}
+        modules={[Pagination, Autoplay, Navigation]}
+      >
         <SwiperSlide>{Slide()}</SwiperSlide>
         <SwiperSlide>{Slide()}</SwiperSlide>
         <SwiperSlide>{Slide()}</SwiperSlide>
@@ -15,6 +28,9 @@ export default function Banner() {
         <SwiperSlide>{Slide()}</SwiperSlide>
         <SwiperSlide>{Slide()}</SwiperSlide>
         <SwiperSlide>{Slide()}</SwiperSlide>
+          <BsFillArrowLeftCircleFill  className="prev hidden sm:block text-center text-secondary leading-relaxed sm:leading-10 text-xl sm:text-2xl absolute top-1/2 left-4 -translate-y-1/2 z-10 rounded-full w-8 sm:w-10 md:w-12 h-8 sm:h-10 md:h-12 border-2 border-secondary cursor-pointer hover:ring hover:ring-offset-2 duration-500 ring-secondary"/>
+        
+          <BsFillArrowRightCircleFill  className="next  hidden sm:block text-center text-secondary leading-relaxed sm:leading-10 text-xl sm:text-2xl absolute top-1/2 right-4 -translate-y-1/2 z-10 rounded-full w-8 sm:w-10 md:w-12 h-8 sm:h-10 md:h-12 border-2 border-secondary cursor-pointer hover:ring hover:ring-offset-2 duration-500 ring-secondary"/>
       </Swiper>
     </div>
   );
@@ -26,7 +42,7 @@ const Slide = () => {
       <div className="w-1/2 pl-2 pr-8">
         <h3 className="text-xl"> Real Estate Agency</h3>
         <h1 className="text-5xl font-bold my-4 text-primary">
-          Find Your Dream <br /> House By Us  f
+          Find Your Dream <br /> House By Us
         </h1>
         <p className="border-l border-primary pl-8">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut
